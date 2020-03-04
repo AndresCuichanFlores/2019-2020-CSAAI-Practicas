85392cf9cf943f9237892461c0b441bd8aeced42
AndresCuichanFlores <fa.cuichan.2016@alumnos.urjc.es>
2020-03-04 12:56:02 +0100

    prueba02

--- P2/practica2_jacascript.js
+++ P2/practica2_jacascript.js
@@ -1,23 +1,51 @@
 console.log("Ejecutando JS...");
 
-const ejemplo2 = document.getElementById('ejemplo');
+const ejemplo = document.getElementById('ejemplo');
 const boton1 = document.getElementById('boton1');
+const boton2 = document.getElementById('boton2');
+const boton3 = document.getElementById('boton3');
+const boton_suma = document.getElementById('boton_suma');
+const boton_igual = document.getElementById('boton_igual');
 
-console.log(ejemplo2.innerHTML)
+
 console.log(ejemplo.innerHTML)
 
+ var cadena = [];
 
 boton1.onclick = () => {
   console.log("Click!");
+  ejemplo.innerHTML = ejemplo.innerHTML + 1
 
-  //-- Cambiar el color de fondo
+
+
+    //-- Cambiar el color de fondo
   //-- Si no tenia color asignado, poner amarillo
   if (boton1.style.backgroundColor=="") {
     boton1.style.backgroundColor = "yellow";
-    boton1.innerHTML = "+"
+
 
   //-- Ya tiene color: quitarselo
   } else {
     boton1.style.backgroundColor = "";
   }
+}
+
+boton2.onclick = () => {
+  ejemplo.innerHTML = ejemplo.innerHTML + 2
+  cadena.push("2")
+
+
+
+}
+boton3.onclick = () => {
+  ejemplo.innerHTML = ejemplo.innerHTML + 3
+  cadena.push("3")
+}
+boton_suma.onclick = () => {
+  ejemplo.innerHTML = ejemplo.innerHTML + "+"
+
+}
+boton_igual.onclick = () => {
+  ejemplo.innerHTML = ejemplo.innerHTML + "="
+
 }
