f353be2b4784f0c1e2b2812a3747f722d8ef1f8d
AndresCuichanFlores <fa.cuichan.2016@alumnos.urjc.es>
2020-03-05 10:52:45 +0100

    prueba04

--- P2/practica2_jacascript.js
+++ P2/practica2_jacascript.js
@@ -1,80 +1,62 @@
 console.log("Ejecutando JS...");
 
 const display = document.getElementById('display');
-const boton1 = document.getElementById('boton1');
-const boton2 = document.getElementById('boton2');
-const boton3 = document.getElementById('boton3');
 const boton_suma = document.getElementById('boton_suma');
+const boton_resta = document.getElementById('boton_resta');
+const boton_dividir = document.getElementById('boton_dividir');
+const boton_multiplicar = document.getElementById('boton_multiplicar');
+const boton_borrar = document.getElementById('boton_borrar');
 const boton_igual = document.getElementById('boton_igual');
 const clear = document.getElementById("clear")
+const digito = document.getElementsByClassName("digito")
 
 
-/*
-const gui = {
-  display: document.getElementById("display"),
-  boton_inc: document.getElementById("boton_suma"),
-  boton_dec: document.getElementById("boton_igual")
-}
 
-const counter = {
-  valor: 0,
-  inc : function(value) {
-    console.log(this.valor)
-    console.log(counter.valor)
-    this.valor += value;
-    console.log(this.valor)
-    console.log(counter.valor)
-    gui.display.innerHTML = this.valor;
+
+
+for (i=0; i<digito.length; i++) {
+  digito[i].onclick = (ev) => {
+    if (display.innerHTML == 0){
+      display.innerHTML = ev.target.value
+    }else{
+      display.innerHTML += ev.target.value
+    }
   }
 }
 
-gui.boton_inc.onclick = () => {
-  counter.inc(1);
-}
-
-gui.boton_dec.onclick = () =>{
-  counter.inc(-1);
-}
-pepe = "10+22"
-console.log(eval(pepe))
-
-*/
-
-
-
-
-boton1.onclick = () => {
-  if (display.innerHTML == 0){
-    display.innerHTML = boton1.value
-  }else{
-    display.innerHTML += boton1.value
-  }
-}
-
-boton2.onclick = () => {
-  if (display.innerHTML == 0){
-    display.innerHTML = boton2.value
-  }else{
-    display.innerHTML += boton2.value
-  }
-}
-boton3.onclick = () => {
-  if (display.innerHTML == 0){
-    display.innerHTML = boton3.value
-  }else{
-    display.innerHTML += boton3.value
-  }
-}
 clear.onclick = () => {
   display.innerHTML = 0
+}
+
+boton_suma.onclick = (ev) => {
+  display.innerHTML += ev.target.value
+}
+
+boton_resta.onclick = (ev) => {
+  display.innerHTML += ev.target.value
+}
+
+boton_dividir.onclick = (ev) => {
+  display.innerHTML += ev.target.value
+}
+
+boton_multiplicar.onclick = (ev) => {
+  display.innerHTML += ev.target.value
+}
+
+boton_borrar.onclick = () => {
+
+  let oracion = []
+
+  for (i=0; i<display.innerHTML.length-1; i++) {
+  oracion += display.innerHTML[i]
+  }
+  display.innerHTML  = oracion;
 
 }
-boton_suma.onclick = () => {
-  display.innerHTML += boton_suma.value
 
-}
 boton_igual.onclick = () => {
-  console.log(display.innerHTML)
   display.innerHTML = eval(display.innerHTML)
-
+  console.log(display.innerHTML)
+  console.log(display.innerHTML.length)
 }
